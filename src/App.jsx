669a939aa6b016b11f12b4ec151986c4151c7c55
Christopher Bryan <swimmer.chris@gmail.com>
2023-12-18 20:59:13 -0500

    adding updates to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import ContactList  from "./components/ContactList.jsx"
 import './App.css'
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-
+      <ContactList />
     </>
   )
 }
